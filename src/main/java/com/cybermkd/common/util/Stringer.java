305b044994ae0ea00fdcd61ef012081731bff7ae
t-baby <t-baby@besdlab.cn>
2016-08-05 23:16:24 +0800

    增加一些文本工具

--- src/main/java/com/cybermkd/common/util/Stringer.java
+++ src/main/java/com/cybermkd/common/util/Stringer.java
@@ -107,4 +107,40 @@ public class Stringer {
         name = camelCase(name);
         return firstUpperCase(name);
     }
+
+    /**
+     * 字符串为 null 或者为  "" 时返回 true
+     * @param str 待处理字符串
+     * @return
+     */
+    public static boolean isBlank(String str) {
+        return str == null || "".equals(str.trim());
+    }
+
+    /**
+     * 字符串不为 null 而且不为  "" 时返回 true
+     * @param str 待处理字符串
+     * @return
+     */
+    public static boolean notBlank(String str) {
+        return str != null && !"".equals(str.trim());
+    }
+
+    public static boolean notBlank(String... strings) {
+        if (strings == null)
+            return false;
+        for (String str : strings)
+            if (str == null || "".equals(str.trim()))
+                return false;
+        return true;
+    }
+
+    public static boolean notNull(Object... paras) {
+        if (paras == null)
+            return false;
+        for (Object obj : paras)
+            if (obj == null)
+                return false;
+        return true;
+    }
 }
